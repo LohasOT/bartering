dcda3960c2d79093674ea1f71b242d4da22260df
LohasOT <oliverhoang93@gmail.com>
2022-01-06 00:59:40 -0800

    remove category

--- models/index.js
+++ models/index.js
@@ -1,12 +1,6 @@
-const Category = require('./Category.js')
 const Item = require('./Item.js')
 const User = require('./User.js')
 const Note = require('./Note.js')
-
-//categories can have many items
-Category.hasMany(Item, { foreignKey: 'cid' })  
-//items belong to a category 
-Item.belongsTo(Category, { foreignKey: 'cid'}) 
 
 //users can have many items
 User.hasMany(Item, { foreignKey: 'uid' })  
