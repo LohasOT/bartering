682b270e5129e0ded5008223b1a2aa3004786162
LohasOT <oliverhoang93@gmail.com>
2022-01-06 00:26:06 -0800

    code

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const express = require('express')
 const { join } = require('path')
 
 const passport = require('passport')
-const { User, Item, Category, Note } = require('./models') 
-//prevents being signed out on page load
+const { User, Note, Item, Category } = require('./models')
 const { Strategy: JWTStrategy, ExtractJwt } = require('passport-jwt')
 
 const app = express()
@@ -27,19 +26,18 @@ passport.use(new JWTStrategy({
   secretOrKey: process.env.SECRET
 }, async function ({ id }, cb) {
   try {
-    const user = await User.findOne({ where: { id }, include: [Item, Note] }) // add note??
+    const user = await User.findOne({ where: { id }, include: [Item, Note] })
     cb(null, user)
   } catch (err) {
     cb(err, null)
   }
 }))
 
-
 app.use(require('./routes'))
 
 async function init() {
   await require('./db').sync()
-  app.listen(process.env.PORT || 3000);
+  app.listen(process.env.PORT || 3000)
 }
 
 init()
