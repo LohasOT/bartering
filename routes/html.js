8391e0e20b7fa4d362e9f06d68bcfdb4f5067406
LohasOT <oliverhoang93@gmail.com>
2022-01-09 02:53:35 -0800

    added routes to html

---
+++ routes/html.js
@@ -0,0 +1,27 @@
+const router = require('express').Router()
+const { join } = require('path')
+
+router.get('/login', (req, res) => {
+  res.sendFile(join(__dirname, '..', 'public', 'login.html'))
+})
+
+router.get('/home', (req, res) => {
+  res.sendFile(join(__dirname, '..', 'public', 'index.html'))
+})
+
+router.get('/profile', (req, res) => {
+  res.sendFile(join(__dirname, '..', 'public', 'itemPost.html'))
+})
+
+router.get('/register', (req, res) => {
+  res.sendFile(join(__dirname, '..', 'public', 'register.html'))
+})
+
+router.get('/edit', (req, res) => {
+  res.sendFile(join(__dirname, '..', 'public', 'editItem.html'))
+})
+
+router.get('/post', (req, res) => {
+  res.sendFile(join(__dirname, '..', 'public', 'itemPage.html'))
+})
+module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ const router = require('express').Router()
 router.use('/api', require('./itemRoutes.js'))
 router.use('/api', require('./userRoutes.js'))
 router.use('/api', require('./noteRoutes.js'))
-
+router.use('/', require('./html.js'))
 
 module.exports = router
